7505b27f8231c4bdcfdc59abae6d34e74f33b920
David Serrano <75829857+serranoio@users.noreply.github.com>
2024-03-18 19:27:24 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import iphone from "./assets/iphone.webp"
+
+function App() {
+
+
+
+
+  return <main>
+    <h1>KOWABUNGA</h1>
+
+    <img
+
+
+      src={iphone}
+    />
+
+  </main>
+}
+
+export default App
